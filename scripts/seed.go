bd1501e15e57df27202c44a095698ff14f56f0ab
Nathan Azevedo <nathan@Nathans-MacBook-Pro.local>
2023-07-23 11:35:18 -0700

    listing room api

--- scripts/seed.go
+++ scripts/seed.go
@@ -28,16 +28,16 @@ func seedHotel(name string, location string, rating int) {
 	}
 	rooms := []types.Room{
 		{
-			Type:      types.SingleRoomType,
-			BasePrice: 99.99,
+			Size:  "small",
+			Price: 99.99,
 		},
 		{
-			Type:      types.DeluxeRoomType,
-			BasePrice: 199.99,
+			Size:  "normal",
+			Price: 133.99,
 		},
 		{
-			Type:      types.SeaSideRoomType,
-			BasePrice: 122.99,
+			Size:  "kingsize",
+			Price: 222.99,
 		},
 	}
 	insertedHotel, err := hotelStore.Insert(ctx, &hotel)
